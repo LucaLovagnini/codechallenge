59e757be219e25f539275c7ffcf0cc1749208c0f
Luca Lovagnini <lucalova91work@gmail.com>
2025-02-22 11:29:26 +0000

    Initial commit

---
+++ src/main/java/com/inditex/inditexcodechallenge/InditexcodechallengeApplication.java
@@ -0,0 +1,12 @@
+package com.inditex.inditexcodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexcodechallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InditexcodechallengeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/inditex/inditexcodechallenge/InditexcodechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.inditexcodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexcodechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
